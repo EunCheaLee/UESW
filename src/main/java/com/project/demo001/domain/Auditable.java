ae311ee5ab0b95aeff959c7d9bf18707fc09c881
EunCheaLee <okwbbg@gmail.com>
2025-04-21 12:27:39 +0900

    Delete src directory

--- src/main/java/com/project/demo001/domain/Auditable.java
+++
@@ -1,8 +0,0 @@
-package com.project.demo001.domain;
-
-import java.time.LocalDateTime;
-
-public interface Auditable {
-	void setCreateAt(LocalDateTime createAt);
-	void setLastConnect(LocalDateTime lastConnect);
-}
